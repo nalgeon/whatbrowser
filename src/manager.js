b576ae32e28df2f1f6b1f3bc25652b5aef3c5cab
antonz <m@antonz.ru>
2014-09-05 00:25:26 +0400

    Use long urls when persisting fails

--- src/manager.js
+++ src/manager.js
@@ -1,7 +1,7 @@
 /*
  * Browser info manager module: webiste info persistence
  */
-(function($, Cookies, WhatBrowser) {
+(function($, Cookies, Hashcode, WhatBrowser) {
     'use strict';
 
     var PARSE_BASE_URL = 'https://api.parse.com/1/classes/WhatBrowser',
@@ -9,8 +9,13 @@
                     'X-Parse-Application-Id': 'WxMn71WOwf2RM6s6vYR57Th1rsfRplumcDDaWQxF',
                     'X-Parse-REST-API-Key': 'EA4xA8UNhbUK5eMyHepEJnUWDyCzoMZxr0t1HOmp'
                 },
-        TIMEOUT = 1000;
-    var parseEnabled = true;
+        SAVE_TIMEOUT = 1000,
+        LOAD_TIMEOUT = 2000,
+        BASE_URL = 'http://whatbrowser.ru/';
+
+    function log(message) {
+        // console.log(message);
+    }
 
     /**
      * Get browser info id from url hash or from cookies
@@ -19,7 +24,7 @@
         function get_id_url() {
             var hash = window.location.hash;
             if (hash && hash.substr(0,2) === '#!') {
-                return { value: hash.substr(2), own: false };
+                return { value: hash.substr(2), local: false };
             }
             return null;
         }
@@ -27,92 +32,181 @@
             if (Cookies.enabled) {
                 var id = Cookies.get('whatbrowser')
                 if (id) {
-                    return { value: id, own: true };
+                    return { value: id, local: true };
                 }
             }
             return null;
         }
-        return get_id_url() || get_id_cookies();
+        return get_id_url() || get_id_cookies() || { value: null, local: true };
+    }
+
+    function get_long_link(whatbrowser) {
+        try {
+            var whatbrowser_str = Hashcode.serialize(whatbrowser);
+            return BASE_URL + '#!' + Hashcode.compress_base64(whatbrowser_str);
+        } catch (e) {
+            return '';
+        }
+    }
+
+    function get_short_link(whatbrowser) {
+        return BASE_URL + '#!' + whatbrowser.id;
     }
 
     /**
-     * Persist new browser info to DB.
+     * Create a new browser info
      */
-    function create() {
-        var loader = WhatBrowser.create({ geo: true }),
+    function create(id) {
+        var loader = WhatBrowser.create(),
             promise = $.Deferred();
         
         loader.done(function(whatbrowser) {
-            if (parseEnabled) {
-                // console.log('Saving info');
-                $.ajax(PARSE_BASE_URL, {
-                    contentType: 'application/json',
-                    data: JSON.stringify(whatbrowser),
-                    dataType: 'json',
-                    headers: PARSE_HEADERS,
-                    timeout: TIMEOUT,
-                    type: 'POST'
-                })
-                .done(function(response) {
-                    // console.log('Saved info');
-                    whatbrowser.id = response.objectId;
-                    whatbrowser.link = 'http://whatbrowser.ru/#!' + whatbrowser.id;
-                    // save id to cookies to load it later on page refresh
-                    if (Cookies.enabled) {
-                        Cookies.set('whatbrowser', whatbrowser.id, { expires: 28800 });
-                    }
-                    promise.resolve(whatbrowser);
-                })
-                .fail(function(xhr, status, error) {
-                    // console.log('Failed to save info, status ' + status + ', error ' + error);
-                    promise.reject(whatbrowser, error);  
-                });
+            if (id) {
+                whatbrowser.id = id;
+                whatbrowser.link = get_short_link(whatbrowser);    
             } else {
-                // console.log('Failed to save info, Parse is not initialized');
-                promise.reject(whatbrowser, 'Parse not initialized');
+                whatbrowser.link = get_long_link(whatbrowser);
             }
+            promise.resolve(whatbrowser);
+        });
+
+        return promise;
+    }
+
+    /**
+     * Create a new browser info and persist it to DB.
+     */
+    function create_and_save() {
+        var loader = WhatBrowser.create(),
+            promise = $.Deferred();
+        
+        loader.done(function(whatbrowser) {
+            log('Saving new info');
+            $.ajax(PARSE_BASE_URL, {
+                contentType: 'application/json',
+                data: JSON.stringify(whatbrowser),
+                dataType: 'json',
+                headers: PARSE_HEADERS,
+                timeout: SAVE_TIMEOUT,
+                type: 'POST'
+            })
+            .done(function(response) {
+                log('Saved info');
+                whatbrowser.id = response.objectId;
+                whatbrowser.link = get_short_link(whatbrowser);
+                // save id to cookies to load it later on page refresh
+                if (Cookies.enabled) {
+                    Cookies.set('whatbrowser', whatbrowser.id, { expires: 28800 });
+                }
+                promise.resolve(whatbrowser);
+            })
+            .fail(function(xhr, status, error) {
+                log('Failed to save info, status ' + status + ', error ' + error);
+                whatbrowser.link = get_long_link(whatbrowser);
+                promise.reject(whatbrowser, error);  
+            });
         });
         
         return promise;
     }
 
     /**
-     * Load browser info from DB.
+     * Update existng browser info.
      */
-    function load(id) {
+    function update(id, whatbrowser) {
         var promise = $.Deferred();
-        if (parseEnabled) {
-            // console.log('Loading info #' + id);
-            $.ajax(PARSE_BASE_URL + '/' + id, {
-                contentType: 'application/json',
-                dataType: 'json',
-                headers: PARSE_HEADERS,
-                timeout: TIMEOUT,
-                type: 'GET'
-            })
-            .done(function(info) {
-                // console.log('Loaded info #' + id);
-                var whatbrowser = new WhatBrowser(info);
-                whatbrowser.id = id;
-                whatbrowser.link = 'http://whatbrowser.ru/#!' + whatbrowser.id;
-                promise.resolve(whatbrowser);
-            })
-            .fail(function(xhr, status, error) {
-                // console.log('Failed to load info #' + id + ', status ' + status + ', error ' + error);
-                promise.reject(error);  
-            });
-        } else {
-            // console.log('Failed to load info, Parse is not initialized');
-            promise.reject('Parse not initialized');
+        
+        log('Updating info');
+        $.ajax(PARSE_BASE_URL + '/' + id, {
+            contentType: 'application/json',
+            data: JSON.stringify(whatbrowser),
+            dataType: 'json',
+            headers: PARSE_HEADERS,
+            timeout: SAVE_TIMEOUT,
+            type: 'PUT'
+        })
+        .done(function(response) {
+            log('Updated info');
+            promise.resolve(whatbrowser);
+        })
+        .fail(function(xhr, status, error) {
+            log('Failed to update info, status ' + status + ', error ' + error);
+            promise.reject(whatbrowser, error);  
+        });
+        
+        return promise;
+    }
+
+    /**
+     * Load browser info from serialized string.
+     */
+    function load_from_str(id) {
+        var whatbrowser,
+            whatbrowser_str,
+            promise = $.Deferred();
+
+        log('Loading info from string: ' + id);
+        try {
+            whatbrowser_str = Hashcode.decompress_base64(id);
+            whatbrowser = new WhatBrowser(Hashcode.parse(whatbrowser_str));
+            whatbrowser.link = get_long_link(whatbrowser);
+            whatbrowser.id = whatbrowser.link.substr(whatbrowser.link.indexOf('/#!') + 3);
+            log('Loaded info');
+            promise.resolve(whatbrowser);
+        } catch (e) {
+            log('Failed to load info: ' + e);
+            promise.reject(whatbrowser, 'Failed to load info from string:' + id);
         }
         return promise;
+    }
+
+    /**
+     * Load browser info from DB.
+     */
+    function load_from_db(id) {
+        var promise = $.Deferred();
+        
+        log('Loading info #' + id);
+        $.ajax(PARSE_BASE_URL + '/' + id, {
+            contentType: 'application/json',
+            dataType: 'json',
+            headers: PARSE_HEADERS,
+            timeout: LOAD_TIMEOUT,
+            type: 'GET'
+        })
+        .done(function(info) {
+            log('Loaded info #' + id);
+            var whatbrowser = new WhatBrowser(info);
+            whatbrowser.id = id;
+            whatbrowser.link = get_short_link(whatbrowser);
+            promise.resolve(whatbrowser);
+        })
+        .fail(function(xhr, status, error) {
+            log('Failed to load info #' + id + ', status ' + status + ', error ' + error);
+            promise.reject(error);  
+        });
+
+        return promise;
+    }
+
+    /**
+     * Load browser info from DB or from serialized string.
+     */
+    function load(id) {
+        if (id.length === 10) {
+            return load_from_db(id);
+        } else {
+            return load_from_str(id);
+        }
     }
 
     window.WhatBrowserManager = {
         get_id: get_id,
         create: create,
+        create_and_save: create_and_save,
+        update: update,
         load: load
     }
     return window.WhatBrowserManager;
 
-})(window.jQuery, window.Cookies, window.WhatBrowser, window.Parse);
+})(window.jQuery, window.Cookies, window.Hashcode, window.WhatBrowser);
